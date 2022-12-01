139de7b6aa691fcaf4c50a2c18f54c57d0297838
Ubuntu <ubuntu@ip-172-31-6-28.ec2.internal>
2022-12-01 11:17:57 +0000

    actualizacion

---
+++ scripts/variables.sh
@@ -0,0 +1,12 @@
+#variables de configuracion wordpress
+DB_NAME=db_wordpress
+DB_USER=db_user
+DB_PASSWORD=db_password
+
+#VAriables let's Encrypt
+email=sgyt_fbjho22@kygur.com
+domain=practicawordpress.hopto.org
+
+#configurar variables wordpress en un directorio
+WP_HOME=https://practicawordpress.hopto.org/
+WP_SITEURL=https://practicawordpress.hopto.org/wordpress
