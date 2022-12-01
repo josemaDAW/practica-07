dc7dc2d7aa1a18e0109f9c55f2711aafa42dc2f8
josemaDAW <josemanuelgallurt24@gmail.com>
2022-12-01 16:32:18 +0000

    ip actualizada

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -2,7 +2,7 @@
 DB_NAME=db_wordpress
 DB_USER=db_user
 DB_PASSWORD=db_password
-DB_HOST_PRIVATE_IP=44.197.183.59
+DB_HOST_PRIVATE_IP=44.203.218.90
 
 #VAriables let's Encrypt
 email=sgyt_fbjho22@kygur.com
