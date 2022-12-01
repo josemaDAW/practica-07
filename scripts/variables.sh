fbe2fb7cb07343e658db827f1ed809134b4f6ea7
josemaDAW <josemanuelgallurt24@gmail.com>
2022-12-01 16:21:29 +0000

    backend and frontend

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -2,6 +2,7 @@
 DB_NAME=db_wordpress
 DB_USER=db_user
 DB_PASSWORD=db_password
+DB_HOST_PRIVATE_IP=44.197.183.59
 
 #VAriables let's Encrypt
 email=sgyt_fbjho22@kygur.com
@@ -10,3 +11,4 @@ domain=practicawordpress.hopto.org
 #configurar variables wordpress en un directorio
 WP_HOME=https://practicawordpress.hopto.org/
 WP_SITEURL=https://practicawordpress.hopto.org/wordpress
+
